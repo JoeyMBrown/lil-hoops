7e261c6f7f5a82730a837f6c66123cad288b3850
JoeyMBrown <brnbunch45@yahoo.com>
2023-01-25 09:31:22 -0500

    Create shotcounter component, alter account page.

--- pages/components/ShotCounter.tsx
+++ pages/components/ShotCounter.tsx
@@ -1,11 +1,31 @@
+import { MouseEvent, useEffect } from "react";
+import { useUser, useSupabaseClient, Session } from '@supabase/auth-helpers-react'
+import { Database } from '../utils/database.types'
+type Shots = Database['public']['Tables']['shots']['Row']
+
 const ShotCounter = () => {
+  const supabase = useSupabaseClient<Database>()
+  const user = useUser()
+
+  useEffect(() => {
+    // check DB for shot today with corresponding user ID as primary key.  If exists, disable button. populate some sort of error message.
+    // If no shot, persist dom.
+  })
+
+  function handleSubmit (e: MouseEvent) {
+    e.preventDefault();
+
+    // Update localState with shot submitted for today = true;
+    // Add shot record for today in shots DB.  Be sure to implicilty state makes and misses
+    console.log(e);
+  }
 
   return (
     <div>
         <form>
             <label htmlFor="shotsMade">Shots Made:</label>
             <input type="number" id="shotsMade" name="shotsMade" min="0" max="3"/>
-            <button type="submit">Submit</button>
+            <button type="submit" onClick={(e) => handleSubmit(e)}>Submit</button>
         </form>
     </div>
   )
